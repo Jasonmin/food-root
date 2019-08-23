796a8ef6d3e50b84248d9d395f23bd0347a57a9f
Jasonmin <313410158@qq.com>
2019-08-23 14:14:38 +0800

    first

---
+++ client-iOS/food-root/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  food-root
+//
+//  Created by taiji_zq on 2019/8/23.
+//  Copyright © 2019 taiji_zq. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
